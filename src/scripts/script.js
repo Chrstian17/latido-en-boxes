5d4b13c584cf10094ab0f3d0624f3feee93e90c4
Christian <christiansoto9617@gmail.com>
2026-03-09 21:52:51 +0100

    09/03

---
+++ src/scripts/script.js
@@ -0,0 +1,86 @@
+const swiperTienda = new Swiper('.swiper.swiper-tienda', {
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Todo lo que sea responsive va AQUÍ dentro
+  breakpoints: {
+    420: {
+      slidesPerView: 1.2, // Un poco de la siguiente foto para invitar a deslizar
+      centeredSlides: true,
+       spaceBetween: 12,
+    },
+    640: {
+      slidesPerView: 2.4,
+      spaceBetween: 12,
+      
+    }
+  },
+
+
+  navigation: {
+    // Asegúrate de que estas clases existan en tu HTML
+    nextEl: '.swiper-tienda .is-next', 
+    prevEl: '.swiper-tienda .is-prev',
+  },
+
+
+});
+
+const swiperInicial = new Swiper('.swiper.swiper-inicial', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 12,
+
+  // Todo lo que sea responsive va AQUÍ dentro
+  breakpoints: {
+    420: {
+      slidesPerView: 1.2, // Un poco de la siguiente foto para invitar a deslizar
+    centeredSlides: true,
+     spaceBetween: 12,
+    },
+    640: {
+      slidesPerView: 2.4,
+      spaceBetween: 20,
+    }
+  },
+
+
+  navigation: {
+    // Asegúrate de que estas clases existan en tu HTML
+    nextEl: '.swiper-inicial .is-next', 
+    prevEl: '.swiper-inicial .is-prev',
+  },
+
+
+});
+
+const swiperFull = new Swiper('.swiper.swiper-full', {
+  direction: 'horizontal',
+  loop: true,
+  
+  spaceBetween: 12,
+  centeredSlides: true,
+
+  // Todo lo que sea responsive va AQUÍ dentro
+  breakpoints: {
+    420: {
+      slidesPerView: 1.2, // Un poco de la siguiente foto para invitar a deslizar
+    },
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 12,
+      
+    }
+  },
+
+
+  navigation: {
+    // Asegúrate de que estas clases existan en tu HTML
+    nextEl: '.swiper-full .is-next', 
+    prevEl: '.swiper-full .is-prev',
+  },
+
+
+});
