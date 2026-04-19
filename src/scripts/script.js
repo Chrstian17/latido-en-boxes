75b329a993757205ac69a1e01ba3c2230985b183
Christian <christiansoto9617@gmail.com>
2026-04-19 20:03:02 +0200

    19/04

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,23 +2,26 @@ const swiperTienda = new Swiper('.swiper.swiper-tienda', {
   direction: 'horizontal',
   loop: true,
 slidesPerView: 1.2, 
-centeredSlides: true,
+
 spaceBetween: 12,
 autoHeight: false,
 
-  // Todo lo que sea responsive va AQUÍ dentro
+   // Todo lo que sea responsive va AQUÍ dentro
   breakpoints: {
     420: {
       slidesPerView: 1.2, // Un poco de la siguiente foto para invitar a deslizar
-      centeredSlides: true,
-       spaceBetween: 12,
+    centeredSlides: true,
+     spaceBetween: 12,
     },
-    640: {
+   
+      767: {
+      slidesPerView: 1.5,
+      spaceBetween: 12,
+    },
+     1200: {
       slidesPerView: 2.4,
       spaceBetween: 12,
-      centeredSlides: false,
-      
-    }
+    },
   },
 
 
@@ -44,10 +47,15 @@ const swiperInicial = new Swiper('.swiper.swiper-inicial', {
     centeredSlides: true,
      spaceBetween: 12,
     },
-    640: {
+   
+      767: {
+      slidesPerView: 1.5,
+      spaceBetween: 12,
+    },
+     1200: {
       slidesPerView: 2.4,
-      spaceBetween: 20,
-    }
+      spaceBetween: 12,
+    },
   },
 
 
@@ -65,26 +73,40 @@ const swiperFull = new Swiper('.swiper.swiper-full', {
   loop: true,
   
   spaceBetween: 12,
-  centeredSlides: true,
+  
 
-  // Todo lo que sea responsive va AQUÍ dentro
+   // Todo lo que sea responsive va AQUÍ dentro
   breakpoints: {
     420: {
       slidesPerView: 1.2, // Un poco de la siguiente foto para invitar a deslizar
+    centeredSlides: true,
+     spaceBetween: 12,
     },
-    640: {
-      slidesPerView: 2,
+   
+      767: {
+      slidesPerView:3 ,
       spaceBetween: 12,
-      
-    }
+    },
+     1200: {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 12,
+      centeredSlides: false,
+    },
   },
 
 
   navigation: {
     // Asegúrate de que estas clases existan en tu HTML
-    nextEl: '.swiper-full .is-next', 
-    prevEl: '.swiper-full .is-prev',
+    nextEl: '.swiper-outer .is-next', 
+    prevEl: '.swiper-outer .is-prev',
   },
 
 
 });
+
+
+
+
+
+
